ef9b97186028eb2c4514ba851f8c6164d62533f2
janine pamintuan <jaja.ninnin@gmail.com>
2024-10-07 13:47:40 -0400

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+function mult(num1, num2) {
+    return num1 * num2;
+};
+
+const num1 = 31;
+const num2 = 2;
+const multiply = mult(num1, num2);
+
+const random = Math.ceil(Math.random());
+
+const num3 = 19;
+const num4 = 5;
+const mod = (num3 % num4);
+
+const max = Math.max(5, 10, 15, 20);
